0af38b27437f26e7a4f3936cc0a6625dc3a29953
devleandrocoelho <devleandrocoelho@gmail.com>
2023-08-16 23:29:27 -0300

    migrando para banco de dados o form

--- src/processar_formulario.php
+++ src/processar_formulario.php
@@ -1,4 +1,16 @@
 <?php
+// Configuração de conexão ao banco de dados
+$host = "localhost"; // Host do banco de dados
+$db_user = "id21099851_userdorh"; // Usuário do banco de dados
+$db_pass = "senh@d0RH"; // Senha do banco de dados
+$db_name = "id21099851_bancodorh"; // Nome do banco de dados
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Erro de conexão ao banco de dados: " . $conn->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
@@ -12,19 +24,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $curriculo_destino = "uploads/" . $curriculo_nome;
 
     if (move_uploaded_file($curriculo_tmp, $curriculo_destino)) {
-        // Enviar email com os detalhes do formulário
-        $to = "devleandrocoelho@gmail.com"; // Substitua pelo seu endereço de e-mail
-        $subject = "Nova candidatura de $nome";
-        $message = "Nome: $nome\nE-mail: $email\nCidade: $cidade\nBairro: $bairro\nTelefone: $telefone";
-        $headers = "From: $email";
+        // Inserir dados no banco de dados
+        $sql = "INSERT INTO candidatos (nome, email, cidade, bairro, telefone, curriculo) VALUES ('$nome', '$email', '$cidade', '$bairro', '$telefone', '$curriculo_nome')";
 
-        if (mail($to, $subject, $message, $headers)) {
+        if ($conn->query($sql) === TRUE) {
             echo "Formulário enviado com sucesso. Obrigado!";
         } else {
-            echo "Ocorreu um erro ao enviar o formulário.";
+            echo "Erro ao inserir os dados no banco de dados: " . $conn->error;
         }
     } else {
         echo "Ocorreu um erro ao fazer o upload do currículo.";
     }
 }
+
+$conn->close();
 ?>
